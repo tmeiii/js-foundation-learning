7df76a44c9e65a706daac53ab4a802cf370936fb
tmeiii <maipnt21406c@st.uel.edu.vn>
2024-09-14 14:18:50 +0700

    mock test

--- LeetCode_Exercise/longestCommonPrefix.js
+++ LeetCode_Exercise/longestCommonPrefix.js
@@ -1,17 +1,11 @@
-// function longestCommonPrefix(strs){
-//     let countChar = new Map();
-//     let first = strs[0];
-//     for (const char of first){
-//         countChar.set(char, (countChar.get(char)||0)+1);
-//     }
-//     return countChar;
-// };
+function longestCommonPrefix(strs){
+    let prefix = strs[0];
 
-// console.log(longestCommonPrefix(["flower","flow","flight"]));
-
-strs = ["flower","flow","flight"]
-prefix = strs[0];
-console.log(prefix)
-let out = strs[1].indexOf(prefix);
-console.log(out)
-console.log(prefix.slice(0,-1))
+    for (let i=1; i<strs.length; i++){  
+        while (strs[i].indexOf(prefix) == -1 || strs[i].indexOf(prefix) !== 0){      
+            prefix=prefix.slice(0,-1);
+        } 
+    }
+    
+    return prefix;
+}
